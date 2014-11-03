005ee6b5d124aadc69d2c66d4cdd2bea8e7aa1a2
TaliSm4N <svhero@naver.com>
2014-11-03 19:03:16 +0900

    Revert "multi input"
    This reverts commit 6c816704a52b313a858dc252fcb1d1b858c1142f.

--- Keyboard_1.ino
+++ Keyboard_1.ino
@@ -5,9 +5,7 @@ const int BUTTON_3 = 10;
 boolean running_1 = false;
 boolean running_2 = false;
 boolean running_3 = false;
-int check_1=0;
-int check_2=0;
-int check_3=0;
+int check=0;
 
 void setup()
 {
@@ -20,56 +18,29 @@ void setup()
 void loop()
 {
   
-  if(check_1==0)
+  if(check==0)
   {
     if (digitalRead(BUTTON_1) == HIGH)
     {
       Keyboard.press('j');
       Keyboard.release('j');
-      check_1=1;
+      check=1;
     }
   }
     else if(digitalRead(BUTTON_1) == LOW)
     {
       Keyboard.press('k');
       Keyboard.release('k');
-      check_1=0;
-      delay(10);
+      check=0;
     }
-  
-  if(check_2==0)
+  else if (running_2)
   {
-    if (digitalRead(BUTTON_2) == HIGH)
-    {
-      Keyboard.press('o');
-      Keyboard.release('o');
-      check_2=1;
-    }
+    Keyboard.print("k");
   }
-    else if(digitalRead(BUTTON_2) == LOW)
-    {
-      Keyboard.press('p');
-      Keyboard.release('p');
-      check_2=0;
-      delay(10);
-    }
-    
-  if(check_3==0)
+  else if (running_3)
   {
-    if (digitalRead(BUTTON_3) == HIGH)
-    {
-      Keyboard.press('a');
-      Keyboard.release('a');
-      check_3=1;
-    }
+    Keyboard.print("l");
   }
-    else if(digitalRead(BUTTON_3) == LOW)
-    {
-      Keyboard.press('b');
-      Keyboard.release('b');
-      check_3=0;
-      delay(10);
-    }
 }
 
 
