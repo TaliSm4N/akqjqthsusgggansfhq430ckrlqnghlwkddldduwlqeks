37c9a01f168af19648e7db588eb52dc5af02f381
Malade <oxywriter@gmail.com>
2014-11-16 15:06:12 +0900

    Folder browser
    Customize your directory

---
+++ FolderBrowserDialog/Backup/TestProject/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 폴더지정toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowDialog();
+            string onClicked = folderDialog.SelectedPath;
+        }
+
+        private void 닫기toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ FolderBrowserDialog/TestProject/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TestProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.메뉴1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.닫기toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.폴더지정toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.메뉴4toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.메뉴3toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.메뉴2toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.메뉴1ToolStripMenuItem,
+            this.메뉴2toolStripMenuItem,
+            this.메뉴3toolStripMenuItem,
+            this.메뉴4toolStripMenuItem,
+            this.폴더지정toolStripMenuItem,
+            this.닫기toolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(592, 41);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 메뉴1ToolStripMenuItem
+            // 
+            this.메뉴1ToolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.메뉴1ToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.메뉴1ToolStripMenuItem.Name = "메뉴1ToolStripMenuItem";
+            this.메뉴1ToolStripMenuItem.Size = new System.Drawing.Size(57, 37);
+            this.메뉴1ToolStripMenuItem.Text = "메뉴1";
+            // 
+            // 닫기toolStripMenuItem
+            // 
+            this.닫기toolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.닫기toolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.닫기toolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.닫기toolStripMenuItem.Name = "닫기toolStripMenuItem";
+            this.닫기toolStripMenuItem.Size = new System.Drawing.Size(49, 37);
+            this.닫기toolStripMenuItem.Text = "닫기";
+            this.닫기toolStripMenuItem.Click += new System.EventHandler(this.닫기toolStripMenuItem_Click);
+            // 
+            // 폴더지정toolStripMenuItem
+            // 
+            this.폴더지정toolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.폴더지정toolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.폴더지정toolStripMenuItem.Name = "폴더지정toolStripMenuItem";
+            this.폴더지정toolStripMenuItem.Size = new System.Drawing.Size(77, 37);
+            this.폴더지정toolStripMenuItem.Text = "폴더지정";
+            this.폴더지정toolStripMenuItem.Click += new System.EventHandler(this.폴더지정toolStripMenuItem_Click);
+            // 
+            // 메뉴4toolStripMenuItem
+            // 
+            this.메뉴4toolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.메뉴4toolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.메뉴4toolStripMenuItem.Name = "메뉴4toolStripMenuItem";
+            this.메뉴4toolStripMenuItem.Size = new System.Drawing.Size(57, 37);
+            this.메뉴4toolStripMenuItem.Text = "메뉴4";
+            // 
+            // 메뉴3toolStripMenuItem
+            // 
+            this.메뉴3toolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.메뉴3toolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.메뉴3toolStripMenuItem.Name = "메뉴3toolStripMenuItem";
+            this.메뉴3toolStripMenuItem.Size = new System.Drawing.Size(57, 37);
+            this.메뉴3toolStripMenuItem.Text = "메뉴3";
+            // 
+            // 메뉴2toolStripMenuItem
+            // 
+            this.메뉴2toolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.메뉴2toolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.메뉴2toolStripMenuItem.Name = "메뉴2toolStripMenuItem";
+            this.메뉴2toolStripMenuItem.Size = new System.Drawing.Size(57, 37);
+            this.메뉴2toolStripMenuItem.Text = "메뉴2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(26, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(531, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "폴더지정을 누르면 폴더를 생성 하고 지정 할 수 있는 Dialog 창이 열립니다.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(426, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "By_상점 티스토리";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 168);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "테스트 프로젝트";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴2toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴3toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴4toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 폴더지정toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 닫기toolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
